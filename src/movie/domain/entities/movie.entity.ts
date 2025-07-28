6f2176cfd4e13cfdee7b365a1ac533c9583c0322
Skalefou <skamsagar@gmail.com>
2025-07-28 15:08:49 +0200

    Ajout entity Movie

---
+++ src/movie/domain/entities/movie.entity.ts
@@ -0,0 +1,10 @@
+export class Movie {
+    constructor(
+        public readonly id: string,
+        public readonly title: string,
+        public readonly director: string,
+        public readonly releaseDate: Date,
+        public readonly genre: string,
+        public readonly cast: string[],
+    ) {}
+}
